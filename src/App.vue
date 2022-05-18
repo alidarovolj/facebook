52f4aa6a813c914677ca7d9e73d14dc98e0f89f6
Olzhas <oljasalidarov@gmail.com>
2022-05-18 21:45:22 +0600

    posts post and put

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
   <div id="app">
-    <regLog v-if="currentUser == null"/>
-    <router-view v-else />
+    <regLog v-if="currentUser == null" />
+    <div v-else>
+      <p
+        @click="logout()"
+        class="
+          absolute
+          top-5
+          right-5
+          p-2
+          rounded-lg
+          bg-slate-200
+          text-black text-lg
+          font-bold
+          hover:cursor-pointer hover:bg-black hover:text-white
+        "
+      >
+        Выйти
+      </p>
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-import regLog from './components/RegistrationLogin.vue'
+import regLog from "./components/RegistrationLogin.vue";
 export default {
   components: {
-    regLog
+    regLog,
   },
   data() {
     return {
-      currentUser: localStorage.getItem('loggedUser')
-    }
-  }
-}
+      currentUser: localStorage.getItem("loggedUser"),
+    };
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem("loggedUser");
+      this.$router.go();
+    },
+  },
+};
 </script>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -7,7 +7,7 @@
         </div>
         <div class="flex items-center justify-between py-3">
           <div class="flex items-center">
-            <img class="h-20 w-20 object-cover mr-4" :src="activeUser[0].avatar" alt="">
+            <img class="h-20 w-20 object-cover mr-4 rounded-full" :src="activeUser[0].avatar" alt="">
             <div>
               <p class="text-3xl font-bold">{{ activeUser[0].name + " " + activeUser[0].surname }}</p>
               <p class="font-bold">Друзья: {{ activeUser[0].friends.length }}</p>
@@ -31,10 +31,34 @@
             <span class="font-bold" v-if="activeUser[0].profileInfo.city != ''">{{ activeUser[0].profileInfo.city }}</span>
             <span class="font-bold" v-if="activeUser[0].profileInfo.city === ''">Данные отсутствуют</span></p>
         </div>
-        <div class="w-2/3">
-          <!-- <div v-for="post of posts">
-
-          </div> -->
+        <div class="w-2/3 pl-4">
+          <h2 class="font-bold text-xl mb-5">Публикации</h2>
+          <div class="flex bg-slate-100 p-2 rounded-lg mb-4">
+            <img class="w-10 h-10 my-auto mr-5 rounded-full" :src="activeUser[0].avatar" alt="">
+            <input v-model="newPost.text" class="w-full p-3 rounded-full" type="text" placeholder="Что у вас нового?">
+            <button @click="sendPost()" class="bg-main px-3 ml-3 text-white font-bold rounded-lg">Отправить</button>
+          </div>
+          <div class="flex flex-wrap flex-col-reverse">
+            <div class="bg-slate-100 w-full p-4 rounded-lg mb-4" v-for="(post, index) of userPosts" :key="post.id">
+              <div class="flex items-start mb-3">
+                <img class="w-14 h-auto mr-5 rounded-full" :src="activeUser[0].avatar" alt="">
+                <div>
+                  <p class="text-xl font-bold">{{ activeUser[0].name + " " + activeUser[0].surname }}</p>
+                  <p>{{ post.createdAt }}</p>
+                </div>
+              </div>
+              <div>
+                <p>{{ post.text }}</p>
+                <div class="my-2 flex items-center">
+                  <p class="block my-auto text-whitet bg-main mr-2 text-center text-white rounded-full w-7 h-7">
+                    <i class="fa-regular fa-thumbs-up"></i></p> <span class="font-bold">{{ post.likes.length }}</span></div>
+              </div>
+              <div>
+                <p @click="setLike(index)" :class="{ 'text-blue-500' : post.likes.includes(currentUser) }" 
+                class="text-base font-bold hover:cursor-pointer">Нравится</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -42,39 +66,75 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   name: "ProfileView",
   data() {
     return {
       users: null,
       posts: null,
-      currentUser: localStorage.getItem('loggedUser')
-    }
+      newPost: {
+        user_login: localStorage.getItem("loggedUser"),
+        text: null,
+        likes: [],
+      },
+      currentUser: localStorage.getItem("loggedUser"),
+    };
   },
   computed: {
     activeUser() {
-      if(this.users === null) {
-        return console.log('...')
+      if (this.users === null) {
+        return console.log("...");
       } else {
-        return this.users.filter((e) => e.email === this.currentUser)
+        return this.users.filter((e) => e.email === this.currentUser);
       }
     },
     userPosts() {
-      if(this.posts === null) {
-        return console.log('...')
+      if (this.posts === null) {
+        return console.log("...");
       } else {
-        return this.posts.filter((e) => e.user_login === this.currentUser)
+        return this.posts.filter((e) => e.user_login === this.currentUser);
       }
-    }
+    },
   },
   async created() {
-    let res = await axios.get('https://6282500ded9edf7bd882691b.mockapi.io/users')
-    this.users = res.data
+    let res = await axios.get(
+      "https://6282500ded9edf7bd882691b.mockapi.io/users"
+    );
+    this.users = res.data;
 
-    let posts = await axios.get('https://6282500ded9edf7bd882691b.mockapi.io/posts')
-    this.posts = posts.data
+    let posts = await axios.get(
+      "https://6282500ded9edf7bd882691b.mockapi.io/posts"
+    );
+    this.posts = posts.data;
     // localStorage.clear()
   },
-}
+  methods: {
+    async setLike(id) {
+      let postID = this.userPosts[id].id;
+      if (this.userPosts[id].likes.includes(this.currentUser)) {
+        let myIndex = this.userPosts[id].likes.indexOf(this.currentUser);
+        this.userPosts[id].likes.splice(myIndex, 1);
+        await axios.put(
+          "https://6282500ded9edf7bd882691b.mockapi.io/posts/" + postID,
+          this.userPosts[id]
+        );
+      } else {
+        console.log(postID);
+        this.userPosts[id].likes.push(this.currentUser);
+        await axios.put(
+          "https://6282500ded9edf7bd882691b.mockapi.io/posts/" + postID,
+          this.userPosts[id]
+        );
+      }
+    },
+    async sendPost() {
+      await axios.post(
+        "https://6282500ded9edf7bd882691b.mockapi.io/posts",
+        this.newPost
+      );
+      this.$router.go();
+    },
+  },
+};
 </script>
